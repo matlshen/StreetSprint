946941d143143e1a0db1baaaa22927bc4a33bc50
Myrtte <edw4rdr@icloud.com>
2024-04-10 16:36:48 -0400

    continuation of front end setup

---
+++ client/src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+return(
+    <div>
+        <div className='LP-title-container'>
+            <div className='LP-title-text'>
+                Welcome To StreetSprint
+            </div>
+        </div>
+    </div>
+)
+
+export default LandingPage;
