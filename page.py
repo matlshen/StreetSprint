a2494e70705c4c0b09170de902f33e0e2355d36b
Trevor Turnquist <turnquisttrevor@gmail.com>
2024-04-20 13:55:52 -0400

    Implemented map updating

--- page.py
+++ page.py
@@ -1,14 +1,26 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
-from foliummap import getMap
+import foliummap
 
 # Create a Dash application
 app = dash.Dash(__name__)
 
-def btnPress():
+map = foliummap.Map()
+
+def btnPress(n_clicks):
     completion_time = 50
-    return f"Completion Time: {completion_time}ms"
+
+    coords = [
+        [26.1 + (n_clicks * 0.1), -82.0 + (n_clicks * 0.1)],
+        [26.1 + (n_clicks * 0.1), -82.1 + (n_clicks * 0.1)],
+        [26.1 + (n_clicks * 0.1), -82.2 + (n_clicks * 0.1)],
+        [26.2 + (n_clicks * 0.1), -82.2 + (n_clicks * 0.1)],
+        [26.3 + (n_clicks * 0.1), -82.3 + (n_clicks * 0.1)]
+    ]
+    map.createMap(coords)
+
+    return f"Completion Time: {completion_time}ms", map.getMap()._repr_html_()
 
 # Define the layout of the Dash application
 app.layout = html.Div([
@@ -48,12 +60,12 @@ app.layout = html.Div([
     ),
     html.Button('Begin', id='button', n_clicks=0, style={'margin': '20px auto', 'display': 'block'}),
     html.Div(id='text-container', style={'margin': '20px auto', 'textAlign': 'center'}),
-    html.Div(id='map-container', children=[html.Iframe(id='map', srcDoc=getMap()._repr_html_(), style={'width': '80%', 'height': '100vh', 'border': '0'})], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'height': '100vh'})
+    html.Div(id='map-container', children=[html.Iframe(id='map', srcDoc=map.getMap()._repr_html_(), style={'width': '80%', 'height': '100vh', 'border': '0'})], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'height': '100vh'})
 ])
 
 # Define callback to display text when button is clicked
 @app.callback(
-    Output('text-container', 'children'),
+    [Output('text-container', 'children'), Output('map', 'srcDoc')],
     [Input('button', 'n_clicks')],
     [State('algorithm-dropdown', 'value'),
      State('location1-dropdown', 'value'),
@@ -62,9 +74,9 @@ app.layout = html.Div([
 
 def update_text(n_clicks, algorithm, location1, location2):
     if n_clicks > 0 and algorithm and location1 and location2 and location1 != location2:
-        return btnPress()
+        return btnPress(n_clicks)
     else:
-        return ''
+        return dash.no_update
 
 if __name__ == '__main__':
     app.run_server(debug=True)
