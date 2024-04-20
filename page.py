2158b054f543a4771c5fc1a2d141b11f078d866d
Myrtte <edw4rdr@icloud.com>
2024-04-20 14:51:21 -0400

    Switched to new input system

--- page.py
+++ page.py
@@ -8,7 +8,7 @@ app = dash.Dash(__name__)
 
 map = foliummap.Map()
 
-def btnPress(n_clicks):
+def btnPress(n_clicks, start_location, end_location):
     completion_time = 50
 
     coords = [
@@ -34,29 +34,19 @@ app.layout = html.Div([
         ],
         value=None,
         placeholder='Select an algorithm...',  # Custom placeholder text
-        style={'width': '50%', 'margin': '20px auto'}
+        style={'width': '50%', 'margin': '25px auto'}
     ),
-    dcc.Dropdown(
-        id='location1-dropdown',
-        options=[
-            {'label': 'McDonalds', 'value': 'mcdonalds'},
-            {'label': 'Shands', 'value': 'shands'},
-            {'label': 'The Hub', 'value': 'the-hub'}
-        ],
-        value=None,
-        placeholder='Select start location',  # Custom placeholder text
-        style={'width': '50%', 'margin': '20px auto'}
+    dcc.Input(
+        id='location1-input',
+        type='text',
+        placeholder='Enter start location',
+        style={'width': '35%', 'margin': '25px auto', 'textAlign': 'center', 'alignItems': 'center', 'display': 'block'}
     ),
-    dcc.Dropdown(
-        id='location2-dropdown',
-        options=[
-            {'label': 'McDonalds', 'value': 'mcdonalds'},
-            {'label': 'Shands', 'value': 'shands'},
-            {'label': 'The Hub', 'value': 'the-hub'}
-        ],
-        value=None,
-        placeholder='Select end location',  # Custom placeholder text
-        style={'width': '50%', 'margin': '20px auto'}
+    dcc.Input(
+        id='location2-input',
+        type='text',
+        placeholder='Enter end location',
+        style={'width': '35%', 'margin': '25px auto', 'textAlign': 'center', 'alignItems': 'center', 'display': 'block'}
     ),
     html.Button('Begin', id='button', n_clicks=0, style={'margin': '20px auto', 'display': 'block'}),
     html.Div(id='text-container', style={'margin': '20px auto', 'textAlign': 'center'}),
@@ -68,13 +58,12 @@ app.layout = html.Div([
     [Output('text-container', 'children'), Output('map', 'srcDoc')],
     [Input('button', 'n_clicks')],
     [State('algorithm-dropdown', 'value'),
-     State('location1-dropdown', 'value'),
-     State('location2-dropdown', 'value')]
+     State('location1-input', 'value'),
+     State('location2-input', 'value')]
 )
-
 def update_text(n_clicks, algorithm, location1, location2):
     if n_clicks > 0 and algorithm and location1 and location2 and location1 != location2:
-        return btnPress(n_clicks)
+        return btnPress(n_clicks, location1, location2)
     else:
         return dash.no_update
 
