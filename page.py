9fbbe2ee3999b5bd556ce2d444bca8af361d1193
Myrtte <edw4rdr@icloud.com>
2024-04-20 15:41:26 -0400

    Reworked Landing Page Visuals

--- page.py
+++ page.py
@@ -24,34 +24,40 @@ def btnPress(n_clicks, start_location, end_location):
 
 # Define the layout of the Dash application
 app.layout = html.Div([
-    html.H1("Welcome to StreetSprint", style={'textAlign': 'center'}),
-    dcc.Dropdown(
-        id='algorithm-dropdown',
-        options=[
-            {'label': 'Dijkstra', 'value': 'dijkstra'},
-            {'label': 'Bellman-Ford', 'value': 'bellman-ford'},
-            {'label': 'A*', 'value': 'a-star'}
-        ],
-        value=None,
-        placeholder='Select an algorithm...',  # Custom placeholder text
-        style={'width': '50%', 'margin': '25px auto'}
-    ),
-    dcc.Input(
-        id='location1-input',
-        type='text',
-        placeholder='Enter start location',
-        style={'width': '35%', 'margin': '25px auto', 'textAlign': 'center', 'alignItems': 'center', 'display': 'block'}
-    ),
-    dcc.Input(
-        id='location2-input',
-        type='text',
-        placeholder='Enter end location',
-        style={'width': '35%', 'margin': '25px auto', 'textAlign': 'center', 'alignItems': 'center', 'display': 'block'}
-    ),
-    html.Button('Begin', id='button', n_clicks=0, style={'margin': '20px auto', 'display': 'block'}),
-    html.Div(id='text-container', style={'margin': '20px auto', 'textAlign': 'center'}),
-    html.Div(id='map-container', children=[html.Iframe(id='map', srcDoc=map.getMap()._repr_html_(), style={'width': '80%', 'height': '100vh', 'border': '0'})], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'height': '100vh'})
-])
+    html.Div([
+        html.H1("Welcome to StreetSprint", style={'textAlign': 'center'}),
+        dcc.Dropdown(
+            id='algorithm-dropdown',
+            options=[
+                {'label': 'Dijkstra', 'value': 'dijkstra'},
+                {'label': 'Bellman-Ford', 'value': 'bellman-ford'},
+                {'label': 'A*', 'value': 'a-star'}
+            ],
+            value=None,
+            placeholder='Select an algorithm...',  # Custom placeholder text
+            style={'width': '70%', 'margin': '20px auto'}
+        ),
+        dcc.Input(
+            id='location1-input',
+            type='text',
+            placeholder='Enter start location',
+            style={'width': '55%', 'margin': '20px auto', 'textAlign': 'center', 'display': 'block'}
+        ),
+        dcc.Input(
+            id='location2-input',
+            type='text',
+            placeholder='Enter end location',
+            style={'width': '55%', 'margin': '20px auto', 'textAlign': 'center', 'display': 'block'}
+        ),
+        html.Button('Begin', id='button', n_clicks=0, style={'margin': '20px auto', 'display': 'block'}),
+        html.Div(id='text-container', style={'margin': '20px auto', 'textAlign': 'center'}),
+    ], style={'float': 'left', 'width': '30%', 'margin-top': '12%'}),
+
+    html.Div([
+        html.Div(id='map-container', children=[html.Iframe(id='map', srcDoc=map.getMap()._repr_html_(), style={'width': '100%', 'height': '100vh', 'border': '0'})],
+                style={'height': '100vh'})
+        ], style={'float': 'right', 'width': '70%'})
+    ])
 
 # Define callback to display text when button is clicked
 @app.callback(
